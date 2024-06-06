3171c2c8838d8d727375dd2ac189137181ec6c92
edwinfigueroa15 <edwinfigueroa15@hotmail.com>
2024-06-06 13:20:00 -0500

    Auth user and token Bearer

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->namespace('App\Http\Controllers')->group(function () {
-    Route::apiResource('task', TaskController::class);
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
+    Route::post('login', UserController::class.'@login');
+    Route::post('signup', UserController::class.'@signup');
+
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::apiResource('task', TaskController::class);
+    });
 });
